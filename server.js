d98409bcce647533459746e91d8ddd728038bc58
Jukka Koivu <c2koju00@students.oamk.fi>
2017-04-11 14:10:37 +0300

    server.js created for heroku and mongodb

--- server.js
+++ server.js
@@ -1,25 +1,89 @@
-// Set Up.
-var express  = require('express');
-var app      = express();                            
-var mongoose = require('mongoose');                     
-var morgan = require('morgan');             
-var bodyParser = require('body-parser');    
-var methodOverride = require('method-override'); 
-var cors = require('cors');
- 
-// Configuration
-mongoose.connect('mongodb://localhost/reviewking');
- 
-app.use(morgan('dev'));                                         
-app.use(bodyParser.urlencoded({'extended':'true'}));            
-app.use(bodyParser.json());                                     
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(methodOverride());
-app.use(cors());
- 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var SCHEDULES_COLLECTION = "schedules";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/api/schedules", function(req, res) {
+  db.collection(SCHEDULES_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      returnArray = [];
+      docs.forEach(function (each) {
+        schedules = {"_Id": each._id, "meeting": each.meeting};
+        returnArray.push(person);
+      });
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/schedules", function(req, res) {
+  var newSchedule = req.body;
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
+
+  db.collection(SCHEDULES_COLLECTION).insertOne(newSchedule, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/api/schedules/:id", function(req, res) {
+});
+
+app.put("/api/schedules/:id", function(req, res) {
+});
+
+app.delete("/api/schedules/:id", function(req, res) {
 });
